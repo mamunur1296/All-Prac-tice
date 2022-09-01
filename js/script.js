375d7631770d76c352f8a99cdd6d4d3c982505b1
mamunur1296 <programer597@gmail.com>
2022-09-01 16:02:53 +0600

    jaon data loded

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const dataOfJson = async () => {
+    const red = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const data = await red.json();
+    displayJust3data(data);
+}
+const displayJust3data = photos => {
+    const galaryJson = document.getElementById('galary-json');
+    photos = photos.slice(0, 3);
+    photos.forEach(photo => {
+        const { title, url, thumbnailUrl } = photo;
+        const div = document.createElement('div');
+        div.classList.add("col");
+        div.innerHTML = `
+                    <div class="card">
+                        <img src="${url}" class="card-img-top h-50" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${title.slice(0, 30)}</h5>
+                            <button type="button" class="btn w-100 btn-secondary btn-sm">Vew All</button>
+                        </div>
+                    </div>
+        `;
+        galaryJson.appendChild(div);
+
+    });
+}
+dataOfJson();
+
