c3fc1c9a3b3e68d48ff8097aea35d8df0389282d
mamunur1296 <programer597@gmail.com>
2022-09-01 20:57:40 +0600

    The mele bd

--- js/script.js
+++ js/script.js
@@ -27,3 +27,30 @@ dataOfJson();
 document.getElementById('galary-json').addEventListener("click", function () {
     window.location.href = "json.html";
 })
+// miles bd statert 
+const dataOfTheMeleBD = async () => {
+    const red = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b');
+    const data = await red.json();
+    displayTheMeleBD(data.meals);
+}
+const displayTheMeleBD = photos => {
+    const galaryJson = document.getElementById('galary-Mele');
+    photos = photos.slice(0, 8);
+    photos.forEach(photo => {
+        const { strTags, strMealThumb } = photo;
+        const div = document.createElement('div');
+        div.classList.add("col");
+        div.innerHTML = `
+                    <div class="card">
+                        <img src="${strMealThumb}" class="card-img-top h-50" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${strTags.slice(0, 30)}</h5>
+                            <button id="galary-json" onclick="dataOfJsoninGalarys ()" type="button" class="btn w-100 btn-secondary btn-sm">Vew All</button>
+                        </div>
+                    </div>
+        `;
+        galaryJson.appendChild(div);
+
+    });
+}
+dataOfTheMeleBD();
